866db2cd08ff1fe83f93ab01a6a61f9435909cd7
Duc Nguyen <hoangduc104@gmail.com>
2025-10-06 00:35:19 +0700

    Change project name

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Duc Nguyen Documentation'
+project = 'Duc-Nguyen Documentation'
 copyright = '2025, Duc Nguyen'
 author = 'Duc Nguyen'
 
